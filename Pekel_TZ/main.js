fe3a671ace4e23b3b45354493fdd7da43a050376
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-06-01 13:15:40 +0300

    Add files via upload

--- Pekel_TZ/main.js
+++ Pekel_TZ/main.js
@@ -8,11 +8,16 @@ async function Rsespons(){
     for(let i =0;i<content.length;i++){
      list.insertAdjacentHTML("beforeend",`
      <li class="post">
-     
      <p>"Name": ${content[i].name}</p>
-     <p>"Date": ${content[i].date.date_not_after.date_not_before}</p>
+     <p>"Date": ${content[i].date.date_not_after}</p>
+     <p>"Date": ${content[i].date.date_not_before}</p>
      <p>"Serial_Num:" ${content[i].serial_number}</p>
-     <p>"Value:" ${content[i].value}</p>
+     <p>"Value:" ${content[i].signature_algoritm}</p>
+     <p>"Value:" ${content[i].signature_algoritm.friendly_name}</p>
+     <p>"Value:" ${content[i].signature_algoritm.value}</p>
+     <p>"Name": ${content[i].date.has_private_key}</p>
+     <p>"Name": ${content[i].date.private_key}</p>
+     <p>"Name": ${content[i].date.public_key}</p>
      <p>"Thumbrint": ${content[i].thumbprint}</p>
  </li>`)
     }
