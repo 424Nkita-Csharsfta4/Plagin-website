73a11c112c47cc057aa815184635272250995b9b
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-06-01 12:56:44 +0300

    Add files via upload

--- Pekel_TZ/main.js
+++ Pekel_TZ/main.js
@@ -1,8 +1,23 @@
+async function Rsespons(){
+    let responsr = await fetch('http://localhost:5235/connection')
+    let content = await responsr.json()
+    console.log(content)
 
-fetch('http://localhost:5235/connection')
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-  console.log(data);
-});
+
+    let list = document.querySelector('.posts')
+    for(let i =0;i<content.length;i++){
+     list.insertAdjacentHTML("beforeend",`
+     <li class="post">
+     
+     <p>"Name": ${content[i].name}</p>
+     <p>"Date": ${content[i].date.date_not_after.date_not_before}</p>
+     <p>"Serial_Num:" ${content[i].serial_number}</p>
+     <p>"Value:" ${content[i].value}</p>
+     <p>"Thumbrint": ${content[i].thumbprint}</p>
+ </li>`)
+    }
+}
+Rsespons()
+
+
+
